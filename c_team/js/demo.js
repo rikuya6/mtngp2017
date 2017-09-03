c43ec59a8e29afabd3863c855783cc12b79bc6ff
rikuya6 <rikuya6@gmail.com>
2017-09-03 17:29:39 +0900

    Player classに移動。罫線スプライトを関数に移行

--- c_team/js/demo.js
+++ c_team/js/demo.js
@@ -8,31 +8,6 @@ window.onload = function() {
   game.preload("RZukin.png", "edit_map.png");
 
   game.onload = function() {
-    let rzukin = new Sprite(spriteSize.x, spriteSize.y);
-    rzukin.image = game.assets["RZukin.png"];
-    rzukin.x = 0;
-    rzukin.y = 0;
-    rzukin.frame = 1;
-    rzukin.isMoving = false;
-    rzukin.direction = 0;
-    rzukin.walk = 1;
-
-    let square = new Sprite(320, 320);
-    square.x = 0;
-    square.y = 0;
-
-    let suef = new Surface(320, 320);
-    square.image = suef;
-    let cont = suef.context;
-    cont.beginPath();
-    for(i=-1; i <= 320; i += 33){
-      cont.moveTo(i, 0);
-      cont.lineTo(i, 320);
-      cont.moveTo(0, i);
-      cont.lineTo(320, i);
-    }
-    cont.stroke();
-
     var MapGroup = enchant.Class.mixClasses(Map, Group, true);
     var map = new MapGroup(spriteSize.x, spriteSize.y);
     map.image = game.assets["edit_map.png"];
@@ -78,100 +53,30 @@ window.onload = function() {
       [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
       [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
     ]);
-
-    map.addChild(rzukin);
+    var ruledLine = getRuledLineSprite();
+    var rzukin = new Player(game, map, "RZukin.png");
+    var s = new MapObject(game, map, "RZukin.png");
     map.addChild(foregroundMap);
-    map.addChild(square);
+    map.addChild(ruledLine);
+    map.addChild(rzukin.getSprite());
+    map.addChild(s.getSprite());
     game.rootScene.addChild(map);
 
-    var button = new Button("▶️");
-    button.moveTo(140, 0);
-    game.rootScene.addChild(button);
-
-    button.ontouchstart = function() {
-      this.text = "Running";
-      for(let i = 0; i < 2; i++) {
-        move.moveRight();
-        move.moveDown();
-        move.moveUp();
-        move.moveLeft();
-        move.moveLeft();
-        move.execute();
-      }
-    };
-
-    var s = new MapObject(game, map, "RZukin.png");
-
-    var move = new MoveController();
-    rzukin.addEventListener('enterframe', function() {
-      if (move.hasNextOrder() && !rzukin.isMoving) {
-        switch (move.nextOrder()) {
-          case 0:
-            game.input.up = true;
-            break;
-          case 1:
-            game.input.right = true;
-            break;
-          case 2:
-            game.input.down = true;
-            break;
-          case 3:
-            game.input.left = true;
-            break;
-          default:
-            throw new Error("想定外のorderです。");
-        }
-      }
-      this.frame = this.direction * 3 + this.walk;
-      if (this.isMoving) {
-        this.moveBy(this.vx, this.vy);
-        console.log(this.x, this.y);
-        if (game.frame % 3 != 0) {
-          this.walk++;
-          this.walk %= 3;
-        }
-        if ((this.vx && (this.x) % spriteSize.x == 0) || (this.vy && this.y % spriteSize.y == 0)) {
-          this.isMoving = false;
-          this.walk = 1;
-        }
-      } else {
-        this.vx = this.vy = 0;
-        if (game.input.left) {
-          this.direction = 1;
-          this.vx = -4;
-        } else if (game.input.right) {
-          this.direction = 2;
-          this.vx = 4;
-        } else if (game.input.up) {
-          this.direction = 3;
-          this.vy = -4;
-        } else if (game.input.down) {
-          this.direction = 0;
-          this.vy = 4;
-        }
-        game.input.right = game.input.left = game.input.up = game.input.down = false;
-        if (this.vx || this.vy) {
-          var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * spriteSize.x : 0) + spriteSize.x;
-          var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * spriteSize.y : 0) + spriteSize.y;
-          if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
-            this.isMoving = true;
-            arguments.callee.call(this);
-          }else{
-            move.stop();
-            alert("この先には進めません!");
-          }
-        }
-      }
-    });
-
-    // game.rootScene.addEventListener('enterframe', function(e) {
-    //   var x = Math.min((game.width - 16) / 2 - player.x, 0);
-    //   var y = Math.min((game.height - 16) / 2 - player.y, 0);
-    //   x = Math.max(game.width, x + map.width) - map.width;
-    //   y = Math.max(game.height, y + map.height) - map.height;
-    //   map.x = x;
-    //   map.y = y;
-    // });
+    // var button = new Button("▶️");
+    // button.moveTo(140, 0);
+    // game.rootScene.addChild(button);
+    //
+    // button.ontouchstart = function() {
+    //   this.text = "Running";
+    //   for(let i = 0; i < 2; i++) {
+    //     rzukin.moveController.moveRight();
+    //     rzukin.moveController.moveDown();
+    //     rzukin.moveController.moveUp();
+    //     rzukin.moveController.moveLeft();
+    //     rzukin.moveController.moveLeft();
+    //     rzukin.moveController.execute();
+    //   }
+    // };
   };
   game.start();
 };
@@ -246,7 +151,7 @@ class MapObject {
     this.beforeY = 0;
     this.sprite.image = game.assets[asset];
     this.sprite.x = 64;
-    map.addChild(this.sprite);
+
     this.sprite.addEventListener(enchant.Event.TOUCH_START, function(e){
       this.originX = e.x - this.x;
       this.originY = e.y - this.y;
@@ -284,4 +189,109 @@ class MapObject {
       map.collisionData[Math.floor(this.y / 32) + 1][Math.floor(this.x / 32) + 1] = 1;  // 現在のマスを当たり判定ありにする
     });
   }
+
+  getSprite() {
+    return this.sprite;
+  }
+}
+
+class Player {
+  constructor(game, map, asset) {
+    this.player = new Sprite(spriteSize.x, spriteSize.y);
+    this.player.image = game.assets[asset];
+    this.player.x = 0;
+    this.player.y = 0;
+    this.player.frame = 1;
+    this.player.isMoving = false;
+    this.player.direction = 0;
+    this.player.walk = 1;
+    this.player.moveController = new MoveController();
+    this.player.move = function () {
+      if (this.moveController.hasNextOrder() && !this.player.isMoving) {
+        switch (this.moveController.nextOrder()) {
+          case 0:
+            game.input.up = true;
+            break;
+          case 1:
+            game.input.right = true;
+            break;
+          case 2:
+            game.input.down = true;
+            break;
+          case 3:
+            game.input.left = true;
+            break;
+          default:
+            throw new Error("想定外のorderです。");
+        }
+      }
+      this.frame = this.direction * 3 + this.walk;
+      if (this.isMoving) {
+        this.moveBy(this.vx, this.vy);
+        console.log(this.x, this.y);
+        if (game.frame % 3 != 0) {
+          this.walk++;
+          this.walk %= 3;
+        }
+        if ((this.vx && (this.x) % spriteSize.x == 0) || (this.vy && this.y % spriteSize.y == 0)) {
+          this.isMoving = false;
+          this.walk = 1;
+        }
+      } else {
+        this.vx = this.vy = 0;
+        if (game.input.left) {
+          this.direction = 1;
+          this.vx = -4;
+        } else if (game.input.right) {
+          this.direction = 2;
+          this.vx = 4;
+        } else if (game.input.up) {
+          this.direction = 3;
+          this.vy = -4;
+        } else if (game.input.down) {
+          this.direction = 0;
+          this.vy = 4;
+        }
+        game.input.right = game.input.left = game.input.up = game.input.down = false;
+        if (this.vx || this.vy) {
+          var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * spriteSize.x : 0) + spriteSize.x;
+          var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * spriteSize.y : 0) + spriteSize.y;
+          if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
+            this.isMoving = true;
+            this.move(); // 描画を1フレーム中に行う
+          }else{
+            this.moveController.stop();
+            alert("この先には進めません!");
+          }
+        }
+      }
+    };
+    this.player.addEventListener('enterframe', function () {
+      this.move();
+    });
+  }
+
+  getSprite() {
+    return this.player;
+  }
+}
+
+function getRuledLineSprite() {
+  // 罫線
+  let square = new Sprite(320, 320);
+  square.x = 0;
+  square.y = 0;
+
+  let suef = new Surface(320, 320);
+  square.image = suef;
+  let cont = suef.context;
+  cont.beginPath();
+  for(let i=-1; i <= 320; i += 33){
+    cont.moveTo(i, 0);
+    cont.lineTo(i, 320);
+    cont.moveTo(0, i);
+    cont.lineTo(320, i);
+  }
+  cont.stroke();
+  return square;
 }
