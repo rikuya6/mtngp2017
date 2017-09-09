184007738fe68dc4dc6e9b2dcda78a7a526e650f
rikuya6 <rikuya6@gmail.com>
2017-09-09 23:11:16 +0900

    削除してしまったweb

---
+++ web/js/base.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+    anchors: ['topPage', 'firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+    menu: '#myMenu',
+    scrollingSpeed: 500
+  });
+});
