2eab8f0b894c0f6772504ccf9543c911cbbc7840
rikuya6 <rikuya6@gmail.com>
2017-09-17 00:16:06 +0900

    button_controller.js

--- c_team/app/assets/javascripts/stage/demo/demo.js
+++ c_team/app/assets/javascripts/stage/demo/demo.js
@@ -7,7 +7,7 @@ function main() {
   gameSize = {x: 320, y: 320};
   spriteSize = {x: 32, y: 32};
 
-  var game = new Game(gameSize.x,gameSize.y);
+  var game = new Game(gameSize.x, gameSize.y);
   game.preload("demo/RZukin.png", "demo/edit_map.png");
 
   game.onload = function() {
@@ -58,7 +58,7 @@ function main() {
     ]);
     var ruledLine = getRuledLineSprite();
     var rzukin = new Player(game, map, "demo/RZukin.png");
-    rzukin.player.addEventListener('enterframe', function () {
+    rzukin.player.addEventListener('enterframe', function() {
       if (this.x == 256 && this.y == 64) {
         Cookies.set('user', { name:'Marcus' }); // データはJSON形式で保存する
         var submit = document.getElementById("demo");
@@ -74,13 +74,8 @@ function main() {
     map.addChild(s.getSprite());
     game.rootScene.addChild(map);
 
-    var button = new Button("▶️");
-    button.moveTo(140, 0);
-    game.rootScene.addChild(button);
-
-    button.ontouchstart = function() {
-      this.text = "Running";
-      for(let i = 0; i < 2; i++) {
+    var button = new ButtonObject("▶️", "Running", function() {
+      for (let i = 0; i < 2; i++) {
         rzukin.player.moveController.moveRight();
         rzukin.player.moveController.moveDown();
         rzukin.player.moveController.moveUp();
@@ -88,10 +83,26 @@ function main() {
         rzukin.player.moveController.moveLeft();
         rzukin.player.moveController.execute();
       }
-    };
+    });
+    button.move(140, 120);
+
+    var resetbutton = new ButtonObject("Reset", "Running", function() {
+      for (let i = 0; i < 2; i++) {
+        rzukin.player.moveController.moveRight();
+        rzukin.player.moveController.moveDown();
+        rzukin.player.moveController.moveUp();
+        rzukin.player.moveController.moveLeft();
+        rzukin.player.moveController.moveLeft();
+        rzukin.player.moveController.execute();
+      }
+    });
+    resetbutton.move(200, 120);
+
+    game.rootScene.addChild(button.getButtonObject());
+    game.rootScene.addChild(resetbutton.getButtonObject());
   };
   game.start();
-};
+}
 
 function getRuledLineSprite() {
   // 罫線
@@ -103,7 +114,7 @@ function getRuledLineSprite() {
   square.image = suef;
   let cont = suef.context;
   cont.beginPath();
-  for(let i=-1; i <= 320; i += 33){
+  for (let i=0; i <= 320; i += 32) {
     cont.moveTo(i, 0);
     cont.lineTo(i, 320);
     cont.moveTo(0, i);
