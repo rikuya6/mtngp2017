ad982a6f2c2a38754f19f0dc3242873d118b5736
M.Ryuichi <leufeng0103@gmail.com>
2017-09-25 00:02:23 +0900

    bootstrapによる制御に切り替え@メニューバー, CSSの全削除

--- web_new/js/main.js
+++ web_new/js/main.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 
-console.log(window.innerWidth);
+// console.log(window.innerWidth);
+
+$(function() {
+  //ページ内スクロール
+  $(".nav-button").click(function () {
+      var i = $(".nav-button").index(this)
+      var p = $(".content-head").eq(i).offset().top - 60;
+      $('html,body').animate({ scrollTop: p }, 500, 'swing');
+      return false;
+  });
+});
