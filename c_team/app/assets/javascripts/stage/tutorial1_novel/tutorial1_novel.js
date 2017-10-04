e8deaf9b5141ffdf912bc99e57803c100c913ff4
Koutai4 <mr_don_katsu_renda_5519@yahoo.co.jp>
2017-10-04 11:59:22 +0900

    わかりにくさを改善💢、画面手直し

--- c_team/app/assets/javascripts/stage/tutorial1_novel/tutorial1_novel.js
+++ c_team/app/assets/javascripts/stage/tutorial1_novel/tutorial1_novel.js
@@ -21,29 +21,104 @@ function main(){
 
     var label = []; // 物語表示のため、配列を用意する。
     var noveltext = [
-      '絵本描き「やぁ。君が僕の本を手伝ってくれるって子かい？」',
+      '絵本描き',
+      '「やぁ。君が僕の本を手伝ってくれるって子かい？',
       false,
-      '絵本描き「僕はここでずっと絵本を書いているんだ。」',
+      '絵本描き',
+      '「僕はここでずっと絵本を書いているんだ。」',
       false,
-      '絵本描き「これから君には僕の絵本の登場人物を動かすのを手伝って',
-      '　　　　　もらうよ。」',
+      '絵本描き',
+      '「これから君には僕の絵本の登場人物を動かすのを手伝って',
+      '　もらうよ。」',
       false,
-      '絵本描き「まずは聞くよりやってみよう。この子を動かして前に進めて',
-      '　　　　　やってくれ。」',
+      '絵本描き',
+      '「まずは聞くよりやってみよう。この子を動かして前に進めて',
+      '　やってくれ。」',
       false,
       'stage1  実行しよう',
       ' ',
       '数マス前のゴールに進むコードが設定されているので、実行を押して',
       'ゴールに行かせてあげよう。',
       false,
-      '？「登場人物は左のブロックの通りに動きます。実行ボタンを押すと',
-      '　　その通りに動くよ。動かしてみよう。」',
+      '？',
+      '「登場人物は左のブロックの通りに動きます。実行ボタンを押すと',
+      '　その通りに動くよ。動かしてみよう。」',
       false,
       false
     ];
 
+    /* 以下からテキストボックスの描画 */
+
+    /* テキストボックス */
+    // Spriteオブジェクトの作成
+    var sprite2 = new Sprite(1112, 200);
+    sprite2.x = 20;
+    sprite2.y = 420;
+    // spriteオブジェクトの背景色の指定
+    sprite2.backgroundColor = "rgba(100, 100, 255, 0.8)";
+    // Surfaceオブジェクトの作成
+    // Spriteの大きさ以上に指定しても範囲外には描画されない
+    var surface = new Surface(100, 100);
+    // SurfaceオブジェクトをSpriteオブジェクトのimageプロパティに代入
+    sprite2.image = surface;
+    // コンテキストを取得する
+    context = surface.context;
+    // パスの描画の初期化
+    context.beginPath();
+    // 描画開始位置の移動
+    context.moveTo(10, 10);
+
+    /* 人名ボックス */
+    var sprite3 = new Sprite(220, 45);
+    sprite3.x = 25;
+    sprite3.y = 392;
+    // spriteオブジェクトの背景色の指定
+    sprite3.backgroundColor = "rgba(255, 100, 255, 0.8)";
+    // Surfaceオブジェクトの作成
+    // Spriteの大きさ以上に指定しても範囲外には描画されない
+    var surface = new Surface(100, 100);
+    // SurfaceオブジェクトをSpriteオブジェクトのimageプロパティに代入
+    sprite3.image = surface;
+    // コンテキストを取得する
+    context = surface.context;
+    // パスの描画の初期化
+    context.beginPath();
+    // 描画開始位置の移動
+    context.moveTo(10, 10);
+    // 描画を行う
+    context.stroke();
+    scene.addChild(sprite2);
+    scene.addChild(sprite3);
+    game.pushScene(scene);
+
+    /* 最初のテキストの表示 */
+    while(true){
+      var work = noveltext[0];
+      noveltext.splice(0, 1); // noveltext０番目から１つ削除
+      if (!(work)) break;  // 配列noveltextにはfalseがある。
+      // 以下、通常通りテキストを表示する処理
+      var tex = new Label(work);
+      tex.width = tex_width;
+      label.push(tex); // falseじゃないなら一度に表示する分追加
+    }
+
+    // 表示の処理
+    for(let i = 0; i < label.length; i++){
+      label[i].moveTo( 40, 400 + i * 40);
+      label[i].font = "32px 'メイリオ'";
+      label[i].color = "white";
+      scene.addChild(label[i]);
+      game.pushScene(scene);
+    }
+
+
+    /* どこをクリックしてもすすめるようにするためのすぷらいとくん */
+    var sprite4 = new Sprite(screen_width, screen_height);
+    scene.addChild(sprite4);
+    game.pushScene(scene);
+
     // 画面がクリックされたならば以下が呼び出される
-    sprite.addEventListener('touchstart', function() {
+    sprite4.addEventListener('touchstart', function() {
       // 既に表示されていた文字を消す
       var len = label.length;
       for(let i = 0; i < len; i++){
@@ -66,7 +141,7 @@ function main(){
 
       // 表示の処理
       for(let i = 0; i < label.length; i++){
-        label[i].moveTo( 40, 440 + i * 40);
+        label[i].moveTo( 40, 400 + i * 40);
         label[i].font = "32px 'メイリオ'";
         label[i].color = "white";
         scene.addChild(label[i]);
@@ -79,7 +154,6 @@ function main(){
         game.pause();
       }
     });
-
   };
   game.start();
   window.scrollTo(0, 0);
