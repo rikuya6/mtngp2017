0bc7d0b530426a69691b62f6e807d237e5046276
M.Ryuichi <leufeng0103@gmail.com>
2017-10-10 11:33:44 +0900

    ボタンの入れ替え, ナビゲーションの色調整

--- web_new/js/main.js
+++ web_new/js/main.js
@@ -5,10 +5,17 @@
 $(function() {
   //ページ内スクロール
   $(".nav-button").click(function () {
-      var i = $(".nav-button").index(this);
-      var p = $(".content-head").eq(i).offset().top - 50;
-      if(i == 0) p = 0;
-      $('html,body').animate({ scrollTop: p }, 500, 'swing');
-      return false;
+    var i = $(".nav-button").index(this);
+    var p = $(".content-head").eq(i).offset().top - 50;
+    if(i == 0) p = 0;
+    $('html,body').animate({ scrollTop: p }, 800, 'swing');
+    return false;
+  });
+
+  $(".nav-triangle").click(function () {
+    var i = $(".nav-triangle").index(this);
+    var p = [$(document).height(), 0];
+    $('html,body').animate({ scrollTop: p[i] }, 800, 'swing');
+    return false;
   });
 });
