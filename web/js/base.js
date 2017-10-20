871592a32dab420faaef045986b13dde7e1ebdb9
Rikuya6 <rikuya6@gmail.com>
2017-10-20 17:01:12 +0900

    web_new を webに置き換え

--- web/js/base.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    verticalCentered: false,
-    anchors: ['topPage', 'firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
-    menu: '#myMenu',
-    scrollingSpeed: 500
-  });
-});
