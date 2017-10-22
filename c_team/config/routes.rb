4ef59e6a232795b268e6b890ce24b0bcea48ee0c
Rikuya6 <rikuya6@gmail.com>
2017-10-22 19:51:56 +0900

    ステージ1 マップ作成

--- c_team/config/routes.rb
+++ c_team/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :users
 
   get 'stages/title', to: 'stages#title', as: :title
+
+  # チュートリアル
   get 'stages/tutorial', to: 'stages#tutorial1', as: :tutorial1
   get 'stages/tutorial2', to: 'stages#tutorial2', as: :tutorial2
   get 'stages/tutorial3', to: 'stages#tutorial3', as: :tutorial3
@@ -14,7 +16,10 @@ Rails.application.routes.draw do
   get 'stages/tutorial2_novel', to: 'stages#tutorial2_novel', as: :tutorial2_novel
   get 'stages/tutorial3_novel', to: 'stages#tutorial3_novel', as: :tutorial3_novel
   get 'stages/tutorial3A_novel', to: 'stages#tutorial3A_novel', as: :tutorial3A_novel
+
+  # 本編
   get 'stages/intro_novel', to: 'stages#intro_novel', as: :intro_novel
+  get 'stages/stage1', to: 'stages#stage1', as: :stage1
   get 'stages/novel2', to: 'stages#novel2', as: :novel2
 
   namespace :admin do
