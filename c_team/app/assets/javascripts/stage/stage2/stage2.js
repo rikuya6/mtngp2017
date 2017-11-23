b1fbfa105f66671e26c83af60094c21d5c8fb727
Rikuya6 <rikuya6@gmail.com>
2017-11-24 00:08:19 +0900

    不要コード削除

--- c_team/app/assets/javascripts/stage/stage2/stage2.js
+++ c_team/app/assets/javascripts/stage/stage2/stage2.js
@@ -137,23 +137,3 @@ function main() {
   };
   game.start();
 }
-
-function getRuledLineSprite() {
-  // 罫線
-  let square = new Sprite(gameSize.x, gameSize.y);
-  square.x = 0;
-  square.y = 0;
-
-  let suef = new Surface(gameSize.x, gameSize.y);
-  square.image = suef;
-  let cont = suef.context;
-  cont.beginPath();
-  for (let i = 0; i <= gameSize.x; i += spriteSize.x) {
-    cont.moveTo(i, 0);
-    cont.lineTo(i, gameSize.x);
-    cont.moveTo(0, i);
-    cont.lineTo(gameSize.x, i);
-  }
-  cont.stroke();
-  return square;
-}
