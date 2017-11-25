ad895b132f56b36f632a9b45a631606413f1eb9f
Rikuya6 <rikuya6@gmail.com>
2017-11-25 21:17:16 +0900

    全てのバス停を順番に通らなかった場合の処理実装

--- c_team/app/assets/javascripts/stage/stage2/stage2.js
+++ c_team/app/assets/javascripts/stage/stage2/stage2.js
@@ -88,15 +88,24 @@ function main() {
       }
       // ゴール(バス停5)
       if (this.x == 256 && this.y == 64) {
-        let status = Cookies.getJSON('status');
-        let addStatus = {
-          stage2: true,
-          bus_stop_flg: bus_stop
-        };
-        Object.assign(status, addStatus);
-        Cookies.set('status', status);
-        submit.submit();
-        game.pause();
+        let reset_flg = false;
+        for (let i = 0; i < bus_stop.length; i++) {
+          if (!bus_stop[i]) reset_flg = true;
+        }
+        if (reset_flg) {
+          // 正しくバス停を通れていない
+          restartStage2(game);
+        } else {
+          let status = Cookies.getJSON('status');
+          let addStatus = {
+            stage2: true,
+            bus_stop_flg: bus_stop
+          };
+          Object.assign(status, addStatus);
+          Cookies.set('status', status);
+          submit.submit();
+          game.pause();
+        }
       }
     });
     map.addChild(foregroundMap);
@@ -125,4 +134,28 @@ function main() {
     game.rootScene.addChild(resetButton.getButtonObject());
   };
   game.start();
+}
+
+function restartStage2(game) {
+  let scene = new Scene();
+  let sprite = new Sprite(1112, 200);
+  sprite.x = 20;
+  sprite.y = 420;
+  sprite.backgroundColor = "rgba(0, 191, 255, 0.8)";
+  scene.addChild(sprite);
+  let noveltext = 'すべての バス停 を 順番に 通ろう!';
+  let textLavel = new Label(noveltext);
+  textLavel.moveTo(40, 400 + 40);
+  textLavel.font = "32px 'メイリオ'";
+  textLavel.color = "white";
+  textLavel.width = 1072;
+  scene.addChild(textLavel);
+  // 画面をクリック出来るようにする
+  let sprite2 = new Sprite(1152, 640);
+  scene.addChild(sprite2);
+  game.pushScene(scene);
+
+  sprite2.addEventListener('touchstart', function () {
+    location.reload();
+  });
 }
