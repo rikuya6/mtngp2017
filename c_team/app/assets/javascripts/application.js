0bbcc3cb8f4ecb6a67bd79346492bf0c90237704
Rikuya6 <rikuya6@gmail.com>
2017-11-25 15:48:55 +0900

    nested_form削除

--- c_team/app/assets/javascripts/application.js
+++ c_team/app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-sprockets
-//= require jquery_nested_form
 //= require enchant/enchant
 //= require enchant/plugins/mixing.enchant
 //= require enchant/plugins/ui.enchant
