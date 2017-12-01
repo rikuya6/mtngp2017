dfa1bcbe59ee3f3909a26e04c3ec88fcbc6fb1db
M.Ryuichi <leufeng0103@gmail.com>
2017-12-01 11:36:47 +0900

    スタイル削除の表現修正

--- c_team/app/assets/javascripts/stage/tutorial1/tutorial1.js
+++ c_team/app/assets/javascripts/stage/tutorial1/tutorial1.js
@@ -103,5 +103,6 @@ function help_button () {
   let helpTextBefore = document.getElementById("help_text_before");
   helpTextBefore.parentNode.removeChild(helpTextBefore);
   let helpTextAfter = document.getElementById("help_text_after");
-  helpTextAfter.style.display = null;
+  // helpTextAfter.style.display = null;
+  helpTextAfter.removeAttribute("style");
 }
